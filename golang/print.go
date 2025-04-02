5223b516273e77585ccbc12b9c5c5b2f21024ab4
deveshpantelwar <devesh.kajla@pantelwar.com>
2025-04-02 17:36:49 +0530

    Add files via upload

---
+++ golang/print.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	age := 22
+	name := "Devesh"
+	height := 5.8234567
+
+	fmt.Println("age:", age, "height: ", height, "name:", name)
+	//age: 25height: 5.8234567name: Alice
+
+	fmt.Println("Hello world")
+
+	// %d integer
+	// %s string
+	// %T type of the value
+	// %.3f number of float digits after dot
+
+	fmt.Printf("age is %d\n", age)
+
+	fmt.Printf("height is %.2f\n", height)
+
+	fmt.Printf("Type of age is %T\n", age)
+
+	fmt.Printf("Type of height is %T\n", height)
+
+	fmt.Printf("name is %s\n", name)
+
+}
