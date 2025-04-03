3ebad425ede91afe641b4b90135b1bb5775b8e10
deveshpantelwar <devesh.kajla@pantelwar.com>
2025-04-03 10:33:35 +0530

    Update print.go

--- golang/print.go
+++ golang/print.go
@@ -19,13 +19,11 @@ func main() {
 	// %.3f number of float digits after dot
 
 	fmt.Printf("age is %d\n", age)
-
 	fmt.Printf("height is %.2f\n", height)
-
 	fmt.Printf("Type of age is %T\n", age)
-
 	fmt.Printf("Type of height is %T\n", height)
-
 	fmt.Printf("name is %s\n", name)
 
+	fmt.Printf("Age is %d\n", age)
+	fmt.Printf("Name: %s, Age: %d, Height: %.2f\n", name, age, height)
 }
