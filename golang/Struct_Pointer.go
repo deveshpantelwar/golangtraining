1c2bc1f73219d963550d15e3cbe2102232e9b7e5
deveshpantelwar <devesh.kajla@pantelwar.com>
2025-04-08 10:47:23 +0530

    Add files via upload

---
+++ golang/Struct_Pointer.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	jim.updateName("jimmy")
+	jim.print()
+
+	jimPointer := &jim
+	jimPointer.updateNamePointer("jimmy")
+	jim.print()
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
+
+//TURN ADDRESS INTO VALUE WITH *ADDRESS
+//TURN VALUE INTO ADDRESS WITH &VALUE
+
+func (pointerToPerson *person) updateNamePointer(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+/*This is a type description -it means we're working with a pointer to a person
+
+func (pointer ToPerson *person) updateName() {
+	*pointer To Person
+}
+This is an operator - it means we want to manipulate the value the pointer is referencing
+
+*/
